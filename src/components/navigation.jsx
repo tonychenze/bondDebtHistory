c89aec46fe8e60bffd9c926f103a50c1181710ec
tonychenze <chenze.tony@gmail.com>
2018-10-19 16:07:51 +0100

    adding the navgation toogle

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,39 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
-const Navigation = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        TradePass
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
+class Navigation extends Component {
+  state = { collapsed: true };
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+  render() {
+    const collapsed = this.state.collapsed;
+    const classOne = collapsed
+      ? "collapse navbar-collapse"
+      : "collapse navbar-collapse show";
+    const classTwo = collapsed
+      ? "navbar-toggler navbar-toggler-right collapsed"
+      : "navbar-toggler navbar-toggler-right";
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          TradePass
+        </Link>
+        <button
+          onClick={this.toggleNavbar}
+          className={`${classTwo}`}
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/risktable">
-              Risk Table
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/monitor">
-              Monitor
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+        <div className={`${classOne}`} id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/risktable">
+                Risk Table
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/monitor">
+                Monitor
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Navigation;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import logger from "./services/logService";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import "./index.css";
